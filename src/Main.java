11b3046f5ffbb3d7b0c240b81b0b095a3f54e43f
APackOfBigAndBamboo <1084150741@qq.com>
2018-09-11 17:05:43 +0800

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import net.sf.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        List list=new ArrayList();
+        JSONObject map = new JSONObject();
+
+        map.put("name", "aaa");
+        map.put("y", "1");
+        list.add(map);
+        list.add(map);
+        list.add(map);
+        list.add(map);
+        System.out.println(list);
     }
 }
